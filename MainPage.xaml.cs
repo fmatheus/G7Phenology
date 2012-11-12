94cdf076d94866c06acdbe802d4f223cb6634918
Fabrício Matheus Gonçalves <fmatheus@gmail.com>
2012-11-12 18:48:19 +0000

    First things first
    MainPage with PhenoTile inception.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Coding4Fun.Phone.Controls;
+
+namespace G7Phenology
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        int count = 0;
+        //bool toogle = true;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello, %s!", "world");
+            ToastPrompt toast = new ToastPrompt();
+            toast.Title = "ToastPrompt" + ++count;
+            toast.Message = "Some message";
+            toast.ImageSource = new BitmapImage(new Uri("ApplicationIcon.png", UriKind.RelativeOrAbsolute));
+            toast.Show();
+        }
+
+        private void Brotamento(Object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Tile has been clicked!" + ((Tile) sender).Content);
+            //((StackPanel) sender.Content).;
+            //Canvas1.Background = new SolidColorBrush(Colors.Blue);
+            //WriteableBitmap icon = new WriteableBitmap((BitmapSource) Brotamento1.Source);
+            //if (toogle)
+            //    ReplaceColor(icon, Colors.White, Colors.Green);
+            //else
+            //    ReplaceColor(icon, Colors.Green, Colors.White);
+            //toogle = !toogle;
+            //Brotamento1.Source = icon;
+        }
+
+        private void ReplaceColor(WriteableBitmap bitmap, Color originalColor, Color replacementColor) {
+            int[] pixels = bitmap.Pixels;
+            int from = originalColor.A << 24 | originalColor.R << 16 | originalColor.G << 8 | originalColor.B;
+            int to = replacementColor.A << 24 | replacementColor.R << 16 | replacementColor.G << 8 | replacementColor.B;
+            for (var x = 0; x < pixels.Length; x++) {
+                if (pixels[x] >> 24 != 0) {
+                    pixels[x] = (BitConverter.GetBytes(pixels[x])[3] << 24)| (to & 0xffffff);
+                }
+            }
+        }
+
+        private void Queda(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PhenoTile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Coding4Fun.Phone.Controls;
+
+namespace G7Phenology
+{
+    public class PhenoTile : Tile
+    {
+        Rectangle icon1;
+        Rectangle icon2;
+
+        public PhenoTile()
+        {
+            Width = 173;
+            Height = 173;
+            TextWrapping = TextWrapping.Wrap;
+            Title = "Brotamento";
+            Background = new SolidColorBrush(Colors.DarkGray);
+            StackPanel icons = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            Content = icons;
+            icon1 = createIcon("Brotamento");
+            icon2 = createIcon("Brotamento");
+            icons.Children.Add(icon1);
+            icons.Children.Add(icon2);
+        }
+
+        Rectangle createIcon(String name)
+        {
+            return new Rectangle
+            {
+                Fill = new SolidColorBrush(Colors.Orange),
+                Width = 62,
+                Height = 62,
+                Margin = new Thickness(6),
+                OpacityMask = new ImageBrush
+                {
+                    ImageSource = new BitmapImage(new Uri("Icons/" + name + ".png", UriKind.RelativeOrAbsolute))
+                }
+            };
+        }
+    }
+}
