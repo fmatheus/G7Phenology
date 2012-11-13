3a12e3ff602dd228c1b036f259014c5dcce45552
Fabrício Matheus Gonçalves <fmatheus@gmail.com>
2012-11-13 05:08:37 +0000

    Cleanup and mockup tests

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -17,52 +17,37 @@ namespace G7Phenology
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        int count = 0;
-        //bool toogle = true;
-        // Constructor
+        int count = -1;
+        static String[] target = { "Phalaenopsis hieroglyphica", "Ophrys tenthredinifera", "Angraecum sesquipedale", "Paphiopedilum concolor" };
+
         public MainPage()
         {
             InitializeComponent();
+            next();
         }
 
-        private void RoundButton_Click(object sender, RoutedEventArgs e)
+        private void OnCheck(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("Hello, %s!", "world");
+            List<int> states = new List<int>(6);
+            for (int i = 0; i < 6; i++)
+            {
+                PhenoTile pheno = (PhenoTile)ContentPanel.Children.ElementAt(i);
+                states.Add(pheno.reset());
+            }
             ToastPrompt toast = new ToastPrompt();
-            toast.Title = "ToastPrompt" + ++count;
-            toast.Message = "Some message";
+            toast.Title = (121 + count).ToString();
+            toast.Message = target[count] + ": " + String.Join(",", states);
             toast.ImageSource = new BitmapImage(new Uri("ApplicationIcon.png", UriKind.RelativeOrAbsolute));
             toast.Show();
-        }
-
-        private void Brotamento(Object sender, RoutedEventArgs e)
+            next();
+       }
+        private void next()
         {
-            //MessageBox.Show("Tile has been clicked!" + ((Tile) sender).Content);
-            //((StackPanel) sender.Content).;
-            //Canvas1.Background = new SolidColorBrush(Colors.Blue);
-            //WriteableBitmap icon = new WriteableBitmap((BitmapSource) Brotamento1.Source);
-            //if (toogle)
-            //    ReplaceColor(icon, Colors.White, Colors.Green);
-            //else
-            //    ReplaceColor(icon, Colors.Green, Colors.White);
-            //toogle = !toogle;
-            //Brotamento1.Source = icon;
+            count = ++count % 4;
+            name.Content = 121 + count + " " + target[count];
+            image.Source = new BitmapImage(new Uri("Images/" + (count+1) + ".jpg", UriKind.RelativeOrAbsolute));
         }
 
-        private void ReplaceColor(WriteableBitmap bitmap, Color originalColor, Color replacementColor) {
-            int[] pixels = bitmap.Pixels;
-            int from = originalColor.A << 24 | originalColor.R << 16 | originalColor.G << 8 | originalColor.B;
-            int to = replacementColor.A << 24 | replacementColor.R << 16 | replacementColor.G << 8 | replacementColor.B;
-            for (var x = 0; x < pixels.Length; x++) {
-                if (pixels[x] >> 24 != 0) {
-                    pixels[x] = (BitConverter.GetBytes(pixels[x])[3] << 24)| (to & 0xffffff);
-                }
-            }
-        }
 
-        private void Queda(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
