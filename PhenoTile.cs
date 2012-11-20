e440516646a47943c70f53132f53b2c6835e1b76
Fabrício Matheus Gonçalves <fmatheus@gmail.com>
2012-11-20 21:41:41 +0000

    Use DependencyProperty to redraw on PhenoPhase intensity change.

--- PhenoTile.cs
+++ PhenoTile.cs
@@ -15,7 +15,12 @@ namespace G7Phenology
 {
     public class PhenoTile : Tile
     {
-        int state = 0;
+        protected static readonly Brush whiteBrush = new SolidColorBrush(Colors.White);
+        protected static readonly String[] phenophases = { "Brotamento", "Queda", "Botão", "Antese", "Fruto Verde", "Fruto Maduro" };
+        protected static Brush[] phenoForeground = new Brush[6];
+        protected static Brush[] phenoBackground = new Brush[6];
+        private static int phase = 0;
+
         Rectangle icon1;
         Rectangle icon2;
 
@@ -27,7 +32,20 @@ namespace G7Phenology
             Margin = new Thickness(12, 12, 0, 0);
         }
 
-        Rectangle createIcon(String name)
+        public int Intensity
+        {
+            get { return (int)GetValue(IntensityProperty); }
+            set { SetValue(IntensityProperty, value); }
+        }
+        public static readonly DependencyProperty IntensityProperty =
+            DependencyProperty.Register("Intensity", typeof(int), typeof(PhenoTile), new PropertyMetadata(0, OnIntensityChange));
+
+        private static void OnIntensityChange(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            (o as PhenoTile).renderIntensity();
+        }
+        
+        static Rectangle createIcon(String name)
         {
             return new Rectangle
             {
@@ -40,17 +58,23 @@ namespace G7Phenology
                 }
             };
         }
-        void renderState()
+        public static Rectangle createIcon(int phenophase)
         {
-            switch (state)
+            Rectangle icon = createIcon(phenophases[phenophase]);
+            icon.Fill = phenoForeground[phenophase];
+            return icon;
+        }
+        private void renderIntensity()
+        {
+            switch (Intensity)
             {
                 case -1:
                     icon1.Fill = Foreground;
-                    icon2.Fill = new SolidColorBrush(Colors.White);
+                    icon2.Fill = whiteBrush;
                     break;
                 case 0:
-                    icon1.Fill = new SolidColorBrush(Colors.White);
-                    icon2.Fill = new SolidColorBrush(Colors.White);
+                    icon1.Fill = whiteBrush;
+                    icon2.Fill = whiteBrush;
                     break;
                 case 1:
                     icon1.Fill = Foreground;
@@ -61,20 +85,12 @@ namespace G7Phenology
             }
         }
 
-        public int reset()
-        {
-            int state = this.state;
-            this.state = 0;
-            renderState();
-            return state;
-        }
-
         protected override void OnClick()
         {
             base.OnClick();
-            state = (state + 1) % 3;
-            renderState();
+            Intensity = (Intensity + 1) % 3;
         }
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
@@ -91,8 +107,10 @@ namespace G7Phenology
                 icon2 = createIcon(Title);
                 icons.Children.Add(icon1);
                 icons.Children.Add(icon2);
-                renderState();
+                renderIntensity();
             }
+            phenoBackground[phase] = Background;
+            phenoForeground[phase++] = Foreground;
         }
     }
 }
